2652f5cedebf47b5a1cb70dba9289e5b41498ca6
LARISSA FIORINI MARTINS <14104127@pucrs.br>
2018-06-28 21:12:18 -0300

    Refactoring

--- t5-infinity-war.pl
+++ t5-infinity-war.pl
@@ -26,7 +26,6 @@ connect(X,Y) :-
 connect(X,Y) :- 
         nextto(Y,X).
 
-% finaliza jogo quando thanos tiver todas joias e tiver em home
 done :-
 	loc(thanos, home),
 	loc(power, thanos),
@@ -35,7 +34,6 @@ done :-
 	loc(reality, thanos),
 	write("Thanos have all the stones!"), nl.
 
-% define pessoas que se pode chase
 heroes :-
 	doctor-strange,
 	spider-man,
@@ -61,20 +59,16 @@ black-widow :-
 	write("The black widow seems to be calling to S.H.I.E.L.D. Be carefull."), nl.
 black-widow.
 
-% move pessoa pra outro lugar. 
 goto(X) :-
 	loc(thanos, L),
 	connect(L, X),
 	move(thanos, X),
-	write("thanos are in the "), write(X), nl.
+	write("Thanos are in the "), write(X), nl.
 goto(_) :-
-	write("thanos can't get there from here."), nl.
+	write("Thanos can't get there from here."), nl.
 
-% comando novo. Attack heroe
-
-% if iron man tem armadura thanos perder, senão pega a pedra.
 attack(iron-man) :-
-	loc(armor,iron-man) -> write("Iron man has his armor. No chance. You've lost Thanos!"), nl, abort ;
+	loc(armor,iron-man) -> write("Iron man has his armor. No chance. You've lost Thanos!"), nl, quit ;
 	loc(iron-man, L),
 	loc(thanos, L),
 	write("Thanos hit doctor-strange.... Congratulations! You now have the reality stone."), nl,
@@ -101,8 +95,6 @@ attack(X):-
 attack(_):-
 	write("The heroe is not here."), nl.
 	
-
-% caça algum heroe. Pode caçar se tiver no mesmo lugar.
 chase(iron-man) :-
 	move(armor,iron-man),
 	write("Oh, Tony Stark apparently is looking for his armor now"), nl.
@@ -113,7 +105,6 @@ chase(black-widow) :-
 	write("The last thing you see is iron man coming in your direction..."), nl,
 	write("The gauntlet is no longer in your hand! Game over Thanos!"), nl,
 	abort.
-	% ACHAR FORMA DE ACABAR PROGRAMA AQUI sem ser com abort
 chase(spider-man) :-
 	loc(spider-man, L),
 	loc(thanos, L),
@@ -123,7 +114,6 @@ chase(spider-man) :-
 chase(_):-
 	write("The heroe is not here."), nl.
 
-% função que pega um item. Pode pegar se pessoa tiver no mesmo lugar q o item.
 take(armor) :-
 	write("Iron man will never let you get close to his armor.."), nl.
 take(power) :-
@@ -134,7 +124,6 @@ take(time) :-
 take(space) :-
 	write("Spider-man has the space stone and it won't give you without some fight..."), nl.
 take(X) :-
-	%X= time -> write("time") ; 
 	loc(thanos, L),
 	loc(X, L),
 	move(X, thanos),
@@ -142,42 +131,39 @@ take(X) :-
 take(X) :-
 	write("There is no "), write(X), write(" here."), nl.
 
-% mostra onde pessoa tá, o que tem e onde pode ir
 look :-
-	write("thanos are in "),
+	write("Thanos are in "),
 	loc(thanos, L), write(L), nl,
 	look_connect(L),
 	look_here(L),
 	look_have(thanos).
 
 look_connect(L) :-
-	write("thanos can go to: "), nl,
+	write("Thanos can go to: "), nl,
 	connect(L, CONNECT),
 	write(" "), write(CONNECT), nl,
 	fail.
 look_connect(_).
 
 look_have(X) :-
-	write("thanos have: "), nl,
+	write("Thanos have: "), nl,
 	loc(THING, X),
 	write(" "), write(THING), nl,
 	fail.
 look_have(_).
 
 look_here(L) :-
-	write("thanos can see: "), nl,
+	write("Thanos can see: "), nl,
 	loc(THING, L),
 	THING \= thanos,
 	write(" "), write(THING), nl,
 	fail.
 look_here(_).
 
-% lista L com elementos que atendem a critério. Mostra todos combinações atuais do sistema.
 report :-
         findall(X:Y, loc(X,Y), L),
         write(L), nl.
 
-% é chamado pela função "go", executa comando solicitado.
 do(goto(X)) :- !, goto(X).
 do(attack(X)) :- !, attack(X).
 do(chase(X)) :- !, chase(X).
@@ -189,7 +175,6 @@ do(listing) :- !, listing.
 do(report) :- !, report.
 do(X) :- write("unknown command"), write(X), nl, instructions.
 
-% controla toda execução do programa
 go :- done.
 go :-
 	write(">> "),
